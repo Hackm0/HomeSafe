81c5f4dd7b67ee97763df82d831c5f8e1a928116
Jason Xa <150358337+XaJason@users.noreply.github.com>
2025-01-25 14:56:51 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+      <h1>PREDECTION</h1>
+      <h2>Building a Sustainable Future Through Property Insights</h2>
+    </div>
+  );
+}
+
+export default App;
