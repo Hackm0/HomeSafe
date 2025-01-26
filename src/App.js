10966a57e55ea4f185b19db91d5fb69e61a294e1
Jason Xa <150358337+XaJason@users.noreply.github.com>
2025-01-25 20:06:38 -0500

    Rearrange map component and connect FE to BE

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import Navbar from './Navbar';
-import React from "react";
-import InteractiveMap from "./components/InteractiveMap";
-import './index.css'; // Facultatif, si vous utilisez un fichier global CSS
+import InteractiveMap from "./components/InteractiveMap"
+
 function App() {
+  const [data, setData] = useState([{}])
+
+  useEffect(() => {
+    fetch("/members")
+    .then(res => res.json())
+    .then(data => {setData(data)
+      console.log(data)
+    }
+  )}, [])
+  
   return (
     <div className="App">
       <Navbar></Navbar>
       <h1>PREDECTION</h1>
       <h2>Building a Sustainable Future Through Property Insights</h2>
-      <InteractiveMap />
+      <InteractiveMap></InteractiveMap>
+        {(typeof data.members === 'undefined') ? (
+            <p>Loading...</p>
+        ) : (
+          data.members.map((member, i) => (
+            <p key={i}>{member}</p>
+          ))
+        )}
     </div>
   );
 }
--- src/flask-server/server.py
+++ src/flask-server/server.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # This will allow all origins by default
 
 #Member API route
 @app.route("/members")
